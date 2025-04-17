61a887d6ddc7ea3e0aec0de0856e1332ae1e933c
xiaoshan <liuhao2495@outlook.com>
2025-04-17 12:48:56 +0800

    递归翻转链表

--- p3.py
+++ p3.py
@@ -17,5 +17,18 @@ class Solution:
             cur = next
 
         return prev
+    
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return None
+        elif head.next == None:
+            return head
+        else:
+            tail = self.reverseList(head.next)
+            head.next.next = head
+            head.next = None
+            return tail
+            
 
             
