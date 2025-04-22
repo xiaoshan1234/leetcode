4f8b3d690fa14de075240931e600922edca75b7d
xiaoshan <liuhao2495@outlook.com>
2025-04-22 15:51:17 +0800

    合并有序链表

---
+++ p8.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        p0 = cur = ListNode()
+        l1 = list1
+        l2 = list2
+        while l1 != None and l2 != None:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+
+            cur = cur.next
+
+        if l1 == None:
+            cur.next = l2
+        else:
+            cur.next = l1
+            
+        return p0.next
